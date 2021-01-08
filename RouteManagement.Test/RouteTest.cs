686890a40ef215dae473b6191a0a5fcfef77900f
Michał Okulewicz <m.okulewicz.phd@gmail.com>
2021-01-08 11:24:25 +0100

    Implemented route with length calculation Fix #1

---
+++ RouteManagement.Test/RouteTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace RouteManagement.Test
+{
+    class RouteTest
+    {
+        [Test]
+        public void LengthTest()
+        {
+            Point p1 = new Point(0, 0);
+            Point p2 = new Point(0, 1);
+            Point p3 = new Point(1, 1);
+            Route route = new Route();
+            Assert.AreEqual(0.0, route.GetLength(), 1e-4);
+            route.AddPoint(p1);
+            Assert.AreEqual(0.0, route.GetLength(), 1e-4);
+            route.AddPoint(p2);
+            Assert.AreEqual(1.0, route.GetLength(), 1e-4);
+            route.AddPoint(p3);
+            Assert.AreEqual(2.0, route.GetLength(), 1e-4);
+        }
+    }
+}
