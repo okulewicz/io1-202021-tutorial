4d499fe8f7eef849d87fc4df9ddae8f65fd7a428
Michał Okulewicz <m.okulewicz.phd@gmail.com>
2021-01-08 11:37:11 +0100

    Generalized distance test with sequential data fix #1

--- RouteManagement.Test/PointTest.cs
+++ RouteManagement.Test/PointTest.cs
@@ -4,18 +4,15 @@ namespace RouteManagement.Test
 {
     public class PointTest
     {
-        [Test]
-        public void DistanceTest()
+        [Test, Sequential]
+        public void DistanceTest([Values(0, 1, 0)] double x,
+            [Values(0, 0, 1)] double y,
+            [Values(0, 1, 1)] double expectedDistance)
         {
             Point referencePoint = new Point(0, 0);
-            double distanceFromSelf = referencePoint.GetDistance(referencePoint);
-            Assert.AreEqual(0.0, distanceFromSelf, 1e-4);
-            Point horizontalPoint = new Point(1, 0);
-            Point verticalPoint = new Point(0, 1);
-            double horizontalDistance = referencePoint.GetDistance(horizontalPoint);
-            double verticalDistance = referencePoint.GetDistance(verticalPoint);
-            Assert.AreEqual(1.0, horizontalDistance, 1e-4);
-            Assert.AreEqual(1.0, verticalDistance, 1e-4);
+            Point otherPoint = new Point(x, y);
+            double distanceFromPoint = referencePoint.GetDistance(otherPoint);
+            Assert.AreEqual(expectedDistance, distanceFromPoint, 1e-4);
         }
     }
 }
